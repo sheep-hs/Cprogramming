38d81924db2a810c639b4d0c20fe6b5cd78b25c4
sheep-hs <loveyhs0620@naver.com>
2026-09-09 17:40:09 +0900

    Update sj 2.c

--- ch12/ch12-2/sj 2.c
+++ ch12/ch12-2/sj 2.c
@@ -3,3 +3,20 @@
 // 날   짜  :  2026년 9월9일
 // 작성자   :  2600115  양효선
 // **********************************************
+
+#include<stdio.h>
+int main(void)
+{
+	int a = -100;
+	char b = 'A';
+	double c = 3.14;
+
+	int* pta = &a;
+	char* ptb = &b;
+	double* ptc = &c;
+
+	printf("int형 변수 a의 값은: %d\n", *pta);
+	printf("char형 변수 b의 값은: %c\n", *ptb);
+	printf("double형 변수 c의 값은: %lf\n", *ptc);
+	return 0;
+}
