69f92975c19bf953e38f9a6e62f014b74b6df94e
sheep-hs <loveyhs0620@naver.com>
2026-09-11 23:27:38 +0900

    Change array representation to pointer representation

--- ch13/ch13-1/sj 2.c
+++ ch13/ch13-1/sj 2.c
@@ -3,6 +3,7 @@
 // 날   짜  :  2026년 9월 11일
 // 작성자   :  2600115  양효선
 // **********************************************
+
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable:6031)
 #include<stdio.h>
