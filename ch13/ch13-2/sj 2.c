227a25de7ef4c12da0ef3e5b11a598b86d645d54
sheep-hs <loveyhs0620@naver.com>
2026-09-18 14:23:56 +0900

    Update sj 2.c
    Add a program to print each character of a string with its index.

--- ch13/ch13-2/sj 2.c
+++ ch13/ch13-2/sj 2.c
@@ -1,1 +1,21 @@
+// **********************************************
+// 제   목  :  널문자를 이용한 각 문자열 개별적 출력
+// 날   짜  :  2026년 9월 18일
+// 작성자   :  2600115  양효선
+// **********************************************
 
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	int i;
+	char str[100];
+	printf("문자열을 입력하시오: ");
+	scanf("%s", str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		printf("%d번째 문자: %c\n", i + 1, str[i]);
+	}
+	return 0;
+}
