ea56deed59f8a65b41ba6d4eb380625a4dc03556
sheep-hs <loveyhs0620@naver.com>
2026-09-18 14:29:58 +0900

    Update sj 3.c
    Implement character case conversion for input strings.

--- ch13/ch13-2/sj 3.c
+++ ch13/ch13-2/sj 3.c
@@ -1,1 +1,26 @@
+// **********************************************
+// 제   목  :  소문자<->대문자 변환 출력
+// 날   짜  :  2026년 9월 18일
+// 작성자   :  2600115  양효선
+// **********************************************
 
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	int i;
+	char str[100];
+	printf("문자열을 입력하시오: ");
+	scanf("%s", str);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	printf("변환 결과: %s", str);
+	return 0;
+}
