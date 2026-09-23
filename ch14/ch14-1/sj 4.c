6c0ae49efb93a5ee5ba76f27cad70c804c3af093
sheep-hs <loveyhs0620@naver.com>
2026-09-23 20:29:09 +0900

    Update sj 4.c
    Implement swap function to exchange values of x, y, and z.

--- ch14/ch14-1/sj 4.c
+++ ch14/ch14-1/sj 4.c
@@ -1,1 +1,31 @@
+// **********************************************
+// 제   목  :  swap함수 활용
+// 날   짜  :  2026년 9월 23일
+// 작성자   :  2600115  양효선
+// **********************************************
 
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+void swap(int* px, int* py);
+int main(void)
+{
+	int x, y, z;
+	printf("정수 x를 입력하시오: ");
+	scanf("%d", &x);
+	printf("정수 y를 입력하시오: ");
+	scanf("%d", &y);
+	printf("정수 z를 입력하시오: ");
+	scanf("%d", &z);
+	printf("swap함수 호출 전 x=%d, y=%d, z=%d\n", x, y, z);
+	swap(&x, &y);
+	swap(&y, &z);
+	printf("swap함수 호출 후 x=%d, y=%d, z=%d", x, y, z);
+}
+void swap(int* px, int* py)
+{
+	int temp;
+	temp = *px;
+	*px = *py;
+	*py = temp;
+}
